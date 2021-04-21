647445b23921666ee1caa18dc205af0959794192
sravan ponugoti <sra1ponugoti08@gmail.com>
2021-04-20 20:40:12 -0400

    learn amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Cloud Computing
+          Learn Amplify
         </a>
       </header>
     </div>
