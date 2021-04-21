f115f31994a910dd6c04d79c4c19d4286cfd5db6
sravan ponugoti <sra1ponugoti08@gmail.com>
2021-04-20 20:29:02 -0400

    learn cloud computing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Cloud Computing
         </a>
       </header>
     </div>
