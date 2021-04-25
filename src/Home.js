1f846353b6dd60caa9634f4f014dc9e21721280f
sravan ponugoti <sra1ponugoti08@gmail.com>
2021-04-25 00:41:12 -0400

    gateway api test

--- src/Home.js
+++ src/Home.js
@@ -8,14 +8,23 @@ function Home() {
     const [menuItems, setMenuItems] = useState([]);
 
     useEffect(() =>
-        fetch("./menu.json", {
+        // fetch("./menu.json", {
+        //     headers : { 
+        //       'Content-Type': 'application/json',
+        //       'Accept': 'application/json'
+        //      }
+        //   })
+        // .then(res => res.json())
+        // .then(res => setMenuItems(res))
+
+        fetch("https://ohczyuo4q4.execute-api.us-east-2.amazonaws.com/my-function", {
             headers : { 
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-             }
-          })
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+               }
+        })
         .then(res => res.json())
-        .then(res => setMenuItems(res))
+        .then(res => console.log(res))
     , []);
 
     return (
