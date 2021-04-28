a0709ebf843e74d1bf6497ce7dfa4d219d7904c6
sravan ponugoti <sra1ponugoti08@gmail.com>
2021-04-28 01:43:37 -0400

    update title

--- src/Home.js
+++ src/Home.js
@@ -85,7 +85,7 @@ function Home(props) {
         {
             menuItems.length ? 
             <>
-                <div className="welcome-banner"><h3>Welcome to the Restaurant Page!</h3> <a href="javascript:void(0)" onClick={logout}>Logout</a></div>
+                <div className="welcome-banner"><h3>Welcome to the Cloud Restaurant!</h3> <a href="javascript:void(0)" onClick={logout}>Logout</a></div>
                 <br></br>
                 <Menu items={menuItems} role={role} addItem={addItem} editItem={editItem} deleteItem={deleteItem} />
             </> : 'Loading...'
